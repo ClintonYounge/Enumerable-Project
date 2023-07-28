373f3d143b95c101f6633d496e58c0bb38fdfa75
Moyasi Ginko <mrsiyam61@gmail.com>
2023-07-29 00:40:56 +0600

    Add MyList class

--- enumerable.rb
+++ enumerable.rb
@@ -16,3 +16,23 @@ module MyEnumerable
   end
 end
 
+class MyList
+  include MyEnumerable
+  def initialize(*elements)
+    @list = elements
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+# Verify our solution
+list = MyList.new(1, 2, 3, 4)
+puts 'Test #all?'
+puts(list.all? { |element| element < 5 }) # Should return true
+puts(list.all? { |element| element > 5 }) # Should return false
+puts "\nTest #any?"
+puts(list.any? { |element| element == 2 }) # Should return true
+puts(list.any? { |element| element == 5 }) # Should return false false
+puts "\nTest #filter"
+git aputs(list.filter(&:even?)) # Should return [2, 4]
